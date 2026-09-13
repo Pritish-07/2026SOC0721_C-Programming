eb4ca87ac08403fb64151fc538d4dc82b178d108
Pritish-07 <pritishmewade@gmail.com>
2026-09-13 21:20:40 +0530

    Add files via upload
    Program 3 uploaded

---
+++ average.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float a,b,c,d;
+    printf("enter the number:");
+    scanf("%f%f%f",&a,&b,&c);
+    c=(a+b+c)/3;
+    printf("average of the number%f",c);
+    return 0;
+
+}

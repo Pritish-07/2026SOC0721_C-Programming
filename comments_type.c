8145accc96ff1c66e125cd67cdb48f900c81f643
Pritish-07 <pritishmewade@gmail.com>
2026-09-25 18:49:16 +0530

    Add files via upload
    4

---
+++ comments_type.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    // This is a single line comment
+    // It explains that the program print a welcome message to the user
+    /* This is a multi-line comment
+       It can span multiple lines
+       Here , we declare a variable and prints its value */
+       int num = 10;// single line comment explaining the variable declaration
+       printf("The value of num is: %d\n", num); 
+       /* End of program
+       Comments do not affect the execution of the program 
+       they are purely for documentation purposes */
+       return 0;
+}
